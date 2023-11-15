56b0c521c2bf511815866339ebe608127d0ebdc0
NGKIPE01 <149111644+NGKIPE01@users.noreply.github.com>
2023-11-15 11:47:57 -0500

    Update standard.py

--- standard.py
+++ standard.py
@@ -1,2 +1,22 @@
 if __name__ == "__main__":
   print("Hello World")
+
+
+def reciprocal(x):
+    """
+    Computes the reciprocal of a given number x.
+
+    Parameters:
+    - x (float): The input number.
+
+    Returns:
+    - float: The reciprocal of x.
+    """
+    if x == 0:
+        raise ValueError("Cannot compute reciprocal for x=0")
+    return 1 / x
+
+# Example usage:
+x_value = 5
+result = reciprocal(x_value)
+print(f"The reciprocal of {x_value} is: {result}")
