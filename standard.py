20a08433b9c0290e1dd182b5412f995576af35a2
Coderdreamgirl <97987523+Coderdreamgirl@users.noreply.github.com>
2023-11-30 16:16:25 -0500

    Update standard.py

--- standard.py
+++ standard.py
@@ -46,34 +46,34 @@ text_box.grid(columnspan=6)
 # start row 2
 
 but_xSqr = tk.Button(calc, text="x^2", command=lambda: add_to_calculation(""), width=5, font=("Arial", 14),
-                     fg="#006B88", bg="#FFFFFF")
+                     fg=INK_BLUE, bg=WHITE)
 but_xSqr.grid(row=2, column=0)
 
 but_div2X = tk.Button(calc, text="2/x", command=lambda: add_to_calculation(""), width=5, font=("Arial", 14),
-                      fg="#006B88", bg="#FFFFFF")
+                      fg=INK_BLUE, bg=WHITE)
 but_div2X.grid(row=2, column=1)
 
 but_clear = tk.Button(calc, text="CE", command=clear_field, width=11, font=("Arial", 14),
-                      fg="#006b88", bg="#FFFFFF")
+                      fg=INK_BLUE, bg=WHITE)
 but_clear.grid(row=2, column=2, columnspan=2)
 # end row 2
 
 # start row 3
 #
 but_open = tk.Button(calc, text="(", command=lambda: add_to_calculation("("), width=5, font=("Arial", 14),
-                     fg="#006B88", bg="#FFFFFF")
+                     fg=INK_BLUE, bg=WHITE)
 but_open.grid(row=3, column=0)
 
 but_close = tk.Button(calc, text=")", command=lambda: add_to_calculation(")"), width=5, font=("Arial", 14),
-                      fg="#006B88", bg="#FFFFFF")
+                      fg=INK_BLUE, bg=WHITE)
 but_close.grid(row=3, column=1)
 
 but_percent = tk.Button(calc, text="%", command=lambda: add_to_calculation("%"), width=5, font=("Arial", 14),
-                        fg="#006B88", bg="#FFFFFF")
+                        fg=INK_BLUE, bg=WHITE)
 but_percent.grid(row=3, column=2)
 # button /
 but_div = tk.Button(calc, text="/", command=lambda: add_to_calculation("/"), width=5, font=("Arial", 14),
-                    fg="#660000", bg="#FFFFFF")
+                    fg=COBALT_GREEN, bg=WHITE)
 but_div.grid(row=3, column=3)
 # end row 3
 
@@ -81,44 +81,44 @@ but_div.grid(row=3, column=3)
 # start row 4
 # number 7
 but7 = tk.Button(calc, text="7", command=lambda: add_to_calculation(7), width=5, font=("Arial", 14),
-                 fg="#7F00FF", bg="#FFFFFF")
+                 fg=VIOLET, bg=WHITE)
 but7.grid(row=4, column=0)
 
 # number 8
 but8 = tk.Button(calc, text="8", command=lambda: add_to_calculation(8), width=5, font=("Arial", 14),
-                 fg="#7F00FF", bg="#FFFFFF")
+                 fg=VIOLET, bg=WHITE)
 but8.grid(row=4, column=1)
 
 # number 9
 but9 = tk.Button(calc, text="9", command=lambda: add_to_calculation(9), width=5, font=("Arial", 14),
-                 fg="#7F00FF", bg="#FFFFFF")
+                 fg=VIOLET, bg=WHITE)
 but9.grid(row=4, column=2)
 
 # button *
 but_multi = tk.Button(calc, text="*", command=lambda: add_to_calculation("*"), width=5, font=("Arial", 14),
-                      fg="#660000", bg="#FFFFFF")
+                      fg=COBALT_GREEN, bg=WHITE)
 but_multi.grid(row=4, column=3)
 # end row 4
 
 # start row 5
 # number 4
 but4 = tk.Button(calc, text="4", command=lambda: add_to_calculation(4), width=5, font=("Arial", 14),
-                 fg="#7F00FF", bg="#FFFFFF")
+                 fg=VIOLET, bg=WHITE)
 but4.grid(row=5, column=0)
 
 # number 5
 but5 = tk.Button(calc, text="5", command=lambda: add_to_calculation(5), width=5, font=("Arial", 14),
-                 fg="#7F00FF", bg="#FFFFFF")
+                 fg=VIOLET, bg=WHITE)
 but5.grid(row=5, column=1)
 
 # number 6
 but6 = tk.Button(calc, text="6", command=lambda: add_to_calculation(6), width=5, font=("Arial", 14),
-                 fg="#7F00FF", bg="#FFFFFF")
+                 fg=VIOLET, bg=WHITE)
 but6.grid(row=5, column=2)
 
 # button -
 but_minus = tk.Button(calc, text="-", command=lambda: add_to_calculation("-"), width=5, font=("Arial", 14),
-                      fg="#660000", bg="#FFFFFF")
+                      fg=COBALT_GREEN, bg=WHITE)
 but_minus.grid(row=5, column=3)
 
 # end row 5
@@ -126,22 +126,22 @@ but_minus.grid(row=5, column=3)
 # start row 6
 # number 1
 but1 = tk.Button(calc, text="1", command=lambda: add_to_calculation(1), width=5, font=("Arial", 14),
-                 fg="#7F00FF", bg="#FFFFFF")
+                 fg=VIOLET, bg=WHITE)
 but1.grid(row=6, column=0)
 
 # number 2
 but2 = tk.Button(calc, text="2", command=lambda: add_to_calculation(2), width=5, font=("Arial", 14),
-                 fg="#7F00FF", bg="#FFFFFF")
+                 fg=VIOLET, bg=WHITE)
 but2.grid(row=6, column=1)
 
 # number 3
 but3 = tk.Button(calc, text="3", command=lambda: add_to_calculation(3), width=5, font=("Arial", 14),
-                 fg="#7F00FF", bg="#FFFFFF")
+                 fg=VIOLET, bg=WHITE)
 but3.grid(row=6, column=2)
 
 # button +
 but_plus = tk.Button(calc, text="+", command=lambda: add_to_calculation("+"), width=5, font=("Arial", 14),
-                     fg="#660000", bg="#FFFFFF")
+                     fg=COBALT_GREEN, bg=WHITE)
 but_plus.grid(row=6, column=3)
 
 # end row 6
@@ -151,15 +151,15 @@ but_plus.grid(row=6, column=3)
 
 # number 0
 but0 = tk.Button(calc, text="0", command=lambda: add_to_calculation(0), width=5, font=("Arial", 14),
-                 fg="#7F00FF", bg="#FFFFFF")
+                 fg=VIOLET, bg=WHITE)
 but0.grid(row=7, column=0)
 
 but_period = tk.Button(calc, text=".", command=lambda: add_to_calculation("."), width=5, font=("Arial", 14),
-                       fg="#7F00FF", bg="#FFFFFF")
+                       fg=VIOLET, bg=WHITE)
 but_period.grid(row=7, column=1)
 
 but_equals = tk.Button(calc, text="=", command=evaluate_calculation, width=11, font=("Arial", 14),
-                       fg="#660000", bg="#FFFFFF")
+                       fg=COBALT_GREEN, bg=WHITE)
 but_equals.grid(row=7, column=2, columnspan=2)
 # end row 7
 
